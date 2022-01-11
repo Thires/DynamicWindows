38ec04945531d3e72d9038ccc39e3ad4420feb12
Thires <thiresdr@gmail.com>
2022-01-10 20:15:49 -0500

    Add files via upload
    fixed label width for tdp plan

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("DynamicWindows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("DynamicWindows")]
+[assembly: AssemblyCopyright("Copyright ?  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("79f27fe4-30ee-4a87-bb34-103496baa106")]
+[assembly: AssemblyFileVersion("4.1.2.0")]
+[assembly: AssemblyVersion("4.1.1075.6819")]
---
+++ Properties/Resources.cs
@@ -0,0 +1,119 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace DynamicWindows.Properties
+{
+  [DebuggerNonUserCode]
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
+  [CompilerGenerated]
+  internal class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (object.ReferenceEquals((object) Resources.resourceMan, (object) null))
+          Resources.resourceMan = new ResourceManager("DynamicWindows.Properties.Resources", typeof (Resources).Assembly);
+        return Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get
+      {
+        return Resources.resourceCulture;
+      }
+      set
+      {
+        Resources.resourceCulture = value;
+      }
+    }
+
+    internal static Bitmap skin_bottom
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_bottom", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_bottomleft
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_bottomleft", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_bottomright
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_bottomright", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_left
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_left", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_right
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_right", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_top
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_top", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_topleft
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_topleft", Resources.resourceCulture);
+      }
+    }
+
+    internal static Bitmap skin_topright
+    {
+      get
+      {
+        return (Bitmap) Resources.ResourceManager.GetObject("skin_topright", Resources.resourceCulture);
+      }
+    }
+
+    internal static Icon Taleweaver
+    {
+      get
+      {
+        return (Icon) Resources.ResourceManager.GetObject("Taleweaver", Resources.resourceCulture);
+      }
+    }
+
+    internal Resources()
+    {
+    }
+  }
+}
