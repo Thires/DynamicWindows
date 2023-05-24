e0b6e863cebf067e49e43a68a7617b31a3a2bcb5
Thires <thiresdr@gmail.com>
2023-05-24 19:04:47 -0400

    Update streamBox.cs

--- streamBox.cs
+++ streamBox.cs
@@ -1,19 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DynamicWindows
-{
-    public class StreamBox
-    {
-        public string Name { get; set; }
-        public string Text { get; set; }
-
-        public StreamBox(string name)
-        {
-            Name = name;
-        }
-    }
-}
