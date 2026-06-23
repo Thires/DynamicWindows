13cef477206370c9c6202634a97377b02e080ac7
Thires <thiresdr@gmail.com>
2026-06-23 12:47:02 -0400

    Add files via upload

--- Properties/Resources.cs
+++ Properties/Resources.cs
@@ -13,120 +13,34 @@ namespace DynamicWindows.Properties
     [CompilerGenerated]
     internal class Resources
     {
-        private static ResourceManager resourceMan;
-        private static CultureInfo resourceCulture;
+        private static ResourceManager? resourceMan;
+        private static CultureInfo? resourceCulture;
 
         [EditorBrowsable(EditorBrowsableState.Advanced)]
         internal static ResourceManager ResourceManager
         {
             get
             {
-                if (object.ReferenceEquals((object)Resources.resourceMan, (object)null))
-                    Resources.resourceMan = new ResourceManager("DynamicWindows.Properties.Resources", typeof(Resources).Assembly);
-                return Resources.resourceMan;
+                resourceMan ??= new ResourceManager("DynamicWindows.Properties.Resources", typeof(Resources).Assembly);
+                return resourceMan;
             }
         }
 
         [EditorBrowsable(EditorBrowsableState.Advanced)]
-        internal static CultureInfo Culture
+        internal static CultureInfo? Culture
         {
-            get
-            {
-                return Resources.resourceCulture;
-            }
-            set
-            {
-                Resources.resourceCulture = value;
-            }
-        }
-
-        internal static Bitmap skin_bottom
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_bottom", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_bottomleft
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_bottomleft", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_bottomright
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_bottomright", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_left
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_left", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_right
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_right", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_top
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_top", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_topleft
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_topleft", Resources.resourceCulture);
-            }
-        }
-
-        internal static Bitmap skin_topright
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("skin_topright", Resources.resourceCulture);
-            }
+            get { return resourceCulture; }
+            set { resourceCulture = value; }
         }
 
         internal static Icon Taleweaver
-        {
-            get
-            {
-                return (Icon)Resources.ResourceManager.GetObject("Taleweaver", Resources.resourceCulture);
-            }
-        }
+            => (Icon)ResourceManager.GetObject("Taleweaver", resourceCulture)!;
 
         internal static Bitmap body_image_ext
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("body_image_ext", Resources.resourceCulture);
-            }
-        }
+            => (Bitmap)ResourceManager.GetObject("body_image_ext", resourceCulture)!;
 
         internal static Bitmap body_image_int
-        {
-            get
-            {
-                return (Bitmap)Resources.ResourceManager.GetObject("body_image_int", Resources.resourceCulture);
-            }
-        }
+            => (Bitmap)ResourceManager.GetObject("body_image_int", resourceCulture)!;
 
         internal Resources()
         {
